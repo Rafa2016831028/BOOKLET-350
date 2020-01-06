2ad3343496169893cbf6d2c6a5bb4095a0f7795c
Rafa2016831028 <jarintasnimaugust20@gmail.com>
2020-01-06 21:09:50 +0600

    removed previous commit
    removed previous commit

--- webprojects/trysql/letstry/letstrydb/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LetstrydbConfig(AppConfig):
-    name = 'letstrydb'
--- webprojects/trysql/letstry/letstrydb/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from . models import Books
-
-class booksSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Books
-		fields= '__all__'
